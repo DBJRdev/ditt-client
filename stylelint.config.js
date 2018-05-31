fa315b6c3001bc7cc0e970b84f1acb223b6bda24
Adam Kudrna <adam@visionapps.cz>
2018-05-31 16:04:38 +0200

    DITT-28: add SCSS Webpack stack

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'stylelint-config-visionapps',
+    'stylelint-config-visionapps-order',
+  ],
+  rules: {
+    indentation: 2,
+    'property-no-unknown': [
+      true, {
+        ignoreProperties: ['composes'],
+      },
+    ],
+  },
+};
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,22 +10,30 @@ module.exports = [{
   entry: {
     bundle: ['babel-polyfill', path.join(__dirname, 'src/main.jsx')],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: ['src', 'node_modules'],
-  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
         include: path.join(__dirname, 'src'),
+        test: /\.(js|jsx)$/,
         use: [{ loader: 'babel-loader' }],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader?importLoaders=2&modules&localIdentName=[name]__[local]___[hash:base64:5]' },
+          { loader: 'sass-loader' },
+        ],
       },
     ],
   },
   output: {
-    publicPath: '/generated/',
-    path: path.join(__dirname, 'public/generated'),
     filename: '[name].js',
+    path: path.join(__dirname, 'public/generated'),
+    publicPath: '/generated/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss'],
+    modules: ['src', 'node_modules'],
   },
 }];
