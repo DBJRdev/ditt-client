b650c62d55be7c54ae092261e46dadd5bec53f98
Bedrich Schindler <bedrich.schindler@gmail.com>
2020-02-12 13:52:35 +0100

    Rework work time calculation logic according to updated calculation logic (#20)

--- src/services/workLogService.js
+++ src/services/workLogService.js
@@ -81,60 +81,121 @@ export const getTypeLabel = (t, type) => {
   return typeLabel;
 };
 
-export const getWorkedTime = (date, workLogList, workHours, workedHoursLimits) => {
-  const correctWorkedSeconds = (workedSeconds) => {
-    if (
-      workedSeconds > workedHoursLimits.lowerLimit.limit
-      && workedSeconds <= workedHoursLimits.upperLimit.limit
-    ) {
-      return workedSeconds + workedHoursLimits.lowerLimit.changeBy;
-    }
+export const getWorkedTime = (workLogList, workHours, workedHoursLimits) => {
+  const standardWorkLogs = [];
+  const businessTripWorkLogs = [];
+  const homeOfficeWorkLogs = [];
+  const sickDayWorkLogs = [];
+  const timeOffWorkLogs = [];
+  const vacationWorkLogs = [];
 
-    if (workedSeconds > workedHoursLimits.upperLimit.limit) {
-      return workedSeconds + workedHoursLimits.upperLimit.changeBy;
-    }
+  let workTime = 0;
+  let workTimeWithoutCorrection = 0;
+  let breakTime = 0;
 
-    return workedSeconds;
-  };
-
-  const businessTripWorkLogs = workLogList.filter((
-    (workLog) => workLog.type === BUSINESS_TRIP_WORK_LOG && workLog.status === STATUS_APPROVED
-  ));
-  const homeOfficeWorkLogs = workLogList.filter((
-    (workLog) => workLog.type === HOME_OFFICE_WORK_LOG && workLog.status === STATUS_APPROVED
-  ));
-  const sickDayWorkLogs = workLogList.filter((
-    (workLog) => workLog.type === SICK_DAY_WORK_LOG
-  ));
-  const vacationWorkLogs = workLogList.filter((
-    (workLog) => workLog.type === VACATION_WORK_LOG && workLog.status === STATUS_APPROVED
-  ));
-
-  // Standard work log time
-  let workedSeconds = workLogList.reduce((total, workLog) => {
+  // Split work logs into arrays by its type and calculate work time of standard work logs.
+  workLogList.forEach((workLog) => {
     if (workLog.type === WORK_LOG) {
-      return (workLog.endTime.diff(workLog.startTime) / 1000) + total;
+      standardWorkLogs.push(workLog);
+
+      // Get work time of current work log
+      const currentWorkTime = workLog.endTime.diff(workLog.startTime) / 1000;
+
+      // Add work time of current work log to total work time.
+      workTimeWithoutCorrection += currentWorkTime;
+
+      // If current work log is longer that 6 hours, 15 minutes break is added.
+      if (currentWorkTime > 21600) {
+        workTime += currentWorkTime - 900;
+        breakTime += 900;
+      } else {
+        workTime += currentWorkTime;
+      }
+    } else if (workLog.type === BUSINESS_TRIP_WORK_LOG && workLog.status === STATUS_APPROVED) {
+      businessTripWorkLogs.push(workLog);
+    } else if (workLog.type === HOME_OFFICE_WORK_LOG && workLog.status === STATUS_APPROVED) {
+      homeOfficeWorkLogs.push(workLog);
+    } else if (workLog.type === SICK_DAY_WORK_LOG) {
+      sickDayWorkLogs.push(workLog);
+    } else if (workLog.type === TIME_OFF_WORK_LOG && workLog.status === STATUS_APPROVED) {
+      timeOffWorkLogs.push(workLog);
+    } else if (workLog.type === VACATION_WORK_LOG && workLog.status === STATUS_APPROVED) {
+      vacationWorkLogs.push(workLog);
     }
+  });
 
-    return total;
-  }, 0);
+  // Calculate break time between standard work logs if there is more than one
+  if (standardWorkLogs.length > 1) {
+    // Sort standard work logs by its start time
+    standardWorkLogs.sort((a, b) => a.startTime.unix() - b.startTime.unix());
 
-  workedSeconds = correctWorkedSeconds(workedSeconds);
+    // Split standard work logs to first element and rest of array
+    // eslint-disable-next-line prefer-const
+    let [previousWorkLog, ...otherWorkLogs] = standardWorkLogs;
 
-  // Special work log time
-  if (
-    workedSeconds === 0
-    && (
-      businessTripWorkLogs.length > 0
-      || homeOfficeWorkLogs.length > 0
-      || sickDayWorkLogs.length > 0
-      || vacationWorkLogs.length > 0
-    )
-  ) {
-    workedSeconds = workHours.get('requiredHours') * 3600;
+    // Calculate break time between standard work logs
+    otherWorkLogs.forEach((standardWorkLog) => {
+      const currentBreakTime = standardWorkLog.startTime.diff(previousWorkLog.endTime) / 1000;
+
+      // Take in account only current break time that is equal or longer that 15 minutes
+      if (currentBreakTime >= 900) {
+        breakTime += currentBreakTime;
+      }
+
+      previousWorkLog = standardWorkLog;
+    });
   }
 
-  return moment.duration({ seconds: workedSeconds });
+  // Correct work and break times according to necessary breaks
+  if (standardWorkLogs.length > 0) {
+    if (
+      workTimeWithoutCorrection > workedHoursLimits.lowerLimit.limit
+      && workTimeWithoutCorrection <= workedHoursLimits.upperLimit.limit
+      && breakTime < Math.abs(workedHoursLimits.lowerLimit.changeBy)
+    ) {
+      const timeToDeduct = Math.abs(workedHoursLimits.lowerLimit.changeBy) - breakTime;
+
+      workTime -= timeToDeduct;
+      breakTime += timeToDeduct;
+    }
+
+    if (
+      workTimeWithoutCorrection > workedHoursLimits.upperLimit.limit
+      && breakTime < Math.abs(workedHoursLimits.upperLimit.changeBy)
+    ) {
+      const timeToDeduct = Math.abs(workedHoursLimits.upperLimit.changeBy) - breakTime;
+
+      workTime -= timeToDeduct;
+      breakTime += timeToDeduct;
+    }
+  }
+
+  // Special work log time calculations
+  if (
+    (
+      standardWorkLogs.length === 0
+      && (
+        businessTripWorkLogs.length > 0
+        || homeOfficeWorkLogs.length > 0
+        || sickDayWorkLogs.length > 0
+        || timeOffWorkLogs.length > 0
+      )
+    ) || vacationWorkLogs.length > 0
+  ) {
+    workTime = workHours.requiredHours * 3600;
+    workTimeWithoutCorrection = workTime;
+    breakTime = 0;
+  } else if (sickDayWorkLogs.length > 0 && standardWorkLogs.length !== 0) {
+    workTime = Math.min(workTimeWithoutCorrection, workHours.requiredHours * 3600);
+    workTimeWithoutCorrection = workTime;
+    breakTime = 0;
+  }
+
+  return {
+    breakTime: moment.duration({ seconds: breakTime }),
+    isWorkTimeCorrected: workTime !== workTimeWithoutCorrection,
+    workTime: moment.duration({ seconds: workTime }),
+  };
 };
 
 export const getWorkLogsByDay = (date, workLogList) => workLogList.filter((workLog) => {
