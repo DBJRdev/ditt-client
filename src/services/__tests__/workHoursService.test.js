d95d3c5119e1cb7c494950188a501229a18b540b
Bedrich Schindler <bedrich.schindler@gmail.com>
2020-05-10 12:52:46 +0200

    Change required and worked hours from hour precision to second precision (#38)

--- src/services/__tests__/workHoursService.test.js
+++ src/services/__tests__/workHoursService.test.js
@@ -5,17 +5,17 @@ import {
 
 describe('getWorkHoursString()', () => {
   it('test expected work hours string', () => {
-    expect(getWorkHoursString(1)).toEqual('1:00');
-    expect(getWorkHoursString(1.5)).toEqual('1:30');
-    expect(getWorkHoursString(1.99)).toEqual('1:59');
+    expect(getWorkHoursString(3600)).toEqual('1:00');
+    expect(getWorkHoursString(5400)).toEqual('1:30');
+    expect(getWorkHoursString(7140)).toEqual('1:59');
   });
 });
 
 describe('getWorkHoursValue()', () => {
   it('test expected work hours value', () => {
-    expect(getWorkHoursValue('1:00')).toEqual(1);
-    expect(getWorkHoursValue('1:30')).toEqual(1.5);
-    expect(getWorkHoursValue('1:59').toFixed(3)).toEqual('1.983');
-    expect(getWorkHoursValue('1:65').toFixed(3)).toEqual('2.083');
+    expect(getWorkHoursValue('1:00')).toEqual(3600);
+    expect(getWorkHoursValue('1:30')).toEqual(5400);
+    expect(getWorkHoursValue('1:59')).toEqual(7140);
+    expect(getWorkHoursValue('1:65')).toEqual(7500);
   });
 });
